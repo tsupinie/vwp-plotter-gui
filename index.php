9748c6b86d47d9dcd70243beb4d43f73325e1cb8
Tim Supinie <tsupinie@gmail.com>
2020-07-25 12:55:09 -0700

    Upgrade to v2.0.1

--- index.php
+++ index.php
@@ -16,11 +16,11 @@ $session_id = md5("{$session_time}{$session_source}");
 <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js'></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="<?php echo $root_url; ?>/map_click_v2.js"></script>
-<script type="text/javascript" src="<?php echo $root_url; ?>/vad/parms_v2.js"></script>
-<script type="text/javascript" src="<?php echo $root_url; ?>/vad/vwp_v2.js"></script>
+<script type="text/javascript" src="<?php echo $root_url; ?>/vad/parms_v2.0.1.js"></script>
+<script type="text/javascript" src="<?php echo $root_url; ?>/vad/vwp_v2.0.1.js"></script>
 <script type="text/javascript" src="<?php echo $root_url; ?>/vad/gifjs/gif.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo $root_url; ?>/main.css.php">
-<link rel="stylesheet" type="text/css" href="<?php echo $root_url; ?>/vad/vwp_v2.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $root_url; ?>/vad/vwp_v2.0.1.css">
 </head>
 <body>
 <input type='hidden' id='root_url' value='<?php echo $root_url; ?>'>
