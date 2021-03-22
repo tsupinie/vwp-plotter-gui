634b1531a7f112ab51a9de45aa0e7e14849305e1
Tim Supinie <tsupinie@gmail.com>
2021-03-21 20:07:13 -0700

    Add lock file and timeout check for get_radar_json.php

--- get_radar_json.php
+++ get_radar_json.php
@@ -1,6 +1,90 @@
 <?php
 
+/*
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+*/
+
 require "../utils.php";
+
+class TimeoutException extends Exception {}
+
+function run_shell_command($cmd, $timeout) {
+    $descriptors = array(
+        0 => array('pipe', 'r'), 
+        1 => array('pipe', 'w'), 
+        2 => array('pipe', 'w')
+    );
+
+    $process = proc_open('exec ' . $cmd, $descriptors, $pipes);
+
+    if (!is_resource($process)) {
+        throw new \Exception('Could not execute process');
+    }
+
+    // Set the stdout and stderr streams to non-blocking.
+    stream_set_blocking($pipes[1], 0);
+    stream_set_blocking($pipes[2], 0);
+
+    // Turn the timeout into microseconds.
+    $timeout = $timeout * 1000000;
+
+    // Output buffer.
+    $buffer = '';
+
+    // While we have time to wait.
+    while ($timeout > 0) {
+        $start = microtime(true);
+
+        // Wait until we have output or the timer expired.
+        $read  = array($pipes[1]);
+        $other = array();
+        stream_select($read, $other, $other, 0, $timeout);
+
+        // Get the status of the process.
+        // Do this before we read from the stream,
+        // this way we can't lose the last bit of output if the process dies between these functions.
+        $status = proc_get_status($process);
+
+        // Read the contents from the buffer.
+        // This function will always return immediately as the stream is non-blocking.
+        $buffer .= stream_get_contents($pipes[1]);
+
+        if (!$status['running']) {
+            // Break from this loop if the process exited before the timeout.
+            break;
+        }
+
+        // Subtract the number of microseconds that we waited.
+        $timeout -= (microtime(true) - $start) * 1000000;
+    }
+
+    // Check if there were any errors.
+    $buffer .= stream_get_contents($pipes[2]);
+
+    $status = proc_get_status($process);
+    $timed_out = false;
+    if ($status['running']) {
+        // Kill the process in case the timeout expired and it's still running.
+        // If the process already exited this won't do anything.
+        proc_terminate($process, 9);
+        $timed_out = true;
+    }
+
+    // Close all streams.
+    fclose($pipes[0]);
+    fclose($pipes[1]);
+    fclose($pipes[2]);
+
+    proc_close($process);
+
+    if ($timed_out) {
+        throw new \TimeoutException();
+    }
+
+    return $buffer;
+}
 
 function get_args() {
     $radar_id = addslashes($_GET['radar']);
@@ -32,7 +116,15 @@ function download_json($args, $output_path) {
     }
 
     $cmd = "/home/autumn35/miniconda3/bin/python $script_path $arg_str 2>&1";
-    $output = shell_exec($cmd);
+    try {
+        $output = run_shell_command($cmd, 15);
+    }
+    catch (TimeoutException $exc) {
+        $output = '{"error": "VWP download timed out"}';
+    }
+    catch (Exception $exc) {
+        $output = "{\"error\": \"{$exc->getMessage()}\"}";
+    }
     return $output;
 }
 
@@ -77,14 +169,28 @@ function _main() {
     $json_path = root_path() . "/vad/json";
     $args = get_args();
 
+    $lock_file_name = "$json_path/{$args['radar']}.{$args['file_id']}.lock";
+
     $json_date = date("Ymd_Hi", strtotime($args['time']));
     $json_fname = "$json_path/{$args['radar']}_$json_date.json";
 
     $output = NULL;
     $already_exists = (file_exists($json_fname) !== false);
+
     if (!$already_exists) {
-        $output = download_json($args, $json_path);
+        while (file_exists($lock_file_name)) {
+            sleep(1);
+        }
+
+        $already_exists = (file_exists($json_fname) !== false);
     }
+
+    if (!$already_exists) {
+        touch($lock_file_name);
+        $output = download_json($args, $json_path);
+        unlink($lock_file_name);
+    }
+
 
     $args['already_exists'] = $already_exists;
     log_visit($args);
