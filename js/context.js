f84ae4d6e9a297b474e66f2e2eca4c7013affa59
Tim Supinie <tsupinie@gmail.com>
2021-07-06 22:42:05 -0700

    Make dpr a public member of the context class

--- js/context.js
+++ js/context.js
@@ -3,7 +3,7 @@ class Context2DWrapper {
     constructor(bbox_pixels, bbox_data, dpr) {
         this.bbox_pixels = bbox_pixels;
         this.bbox_data = bbox_data;
-        this._dpr = dpr;
+        this.dpr = dpr;
     }
 
     get(target, prop) {
@@ -59,7 +59,7 @@ class Context2DWrapper {
         var canvas_rad;
 
         if (rad_units == 'pixels') {
-            canvas_rad = rad * this._dpr;
+            canvas_rad = rad * this.dpr;
         }
         else if (rad_units == 'data') {
             var [x_edge, y_edge] = this.data_to_pix(ctx, x_data + rad, y_data);
@@ -131,8 +131,18 @@ class Context2DWrapper {
 
     }
 
+    measureText(ctx, str) {
+        let obj = {'width': 0};
+
+        str.split("\n").forEach(line => {
+            obj['width'] = Math.max(obj['width'], ctx.measureText(line).width);
+        });
+
+        return obj;
+    }
+
     setLineDash(ctx, segments) {
-        var segs_dpr = segments.map(seg => (seg * this._dpr));
+        var segs_dpr = segments.map(seg => (seg * this.dpr));
         ctx.setLineDash(segs_dpr);
     }
 
@@ -141,7 +151,7 @@ class Context2DWrapper {
     }
 
     __set_lineWidth(ctx, line_width) {
-        return (ctx.lineWidth = this._dpr * line_width);
+        return (ctx.lineWidth = this.dpr * line_width);
     }
 
     __set_font(ctx, font) {
@@ -150,7 +160,7 @@ class Context2DWrapper {
         var font_dpr = font;
 
         if (font_size) {
-            font_size = font_size[0] * this._dpr;
+            font_size = font_size[0] * this.dpr;
             var font_dpr = font.replace(font_size_regex, font_size);
         }
 
@@ -162,18 +172,18 @@ class Context2DWrapper {
         // First argument is a dummy because the get proxy sticks the original drawing context in there. Surely there's
         //  a better way to handle that?
         var [x, y] = this.data_to_pix(_, x_data, y_data);
-        return this.pix_to_data(_, x + x_offset * this._dpr, y + y_offset * this._dpr);
+        return this.pix_to_data(_, x + x_offset * this.dpr, y + y_offset * this.dpr);
     }
 
     data_to_pix(_, x_data, y_data) {
         var x = linear_interp(x_data, this.bbox_data.lbx, this.bbox_data.ubx, this.bbox_pixels.lbx, this.bbox_pixels.ubx);
         var y = linear_interp(y_data, this.bbox_data.lby, this.bbox_data.uby, this.bbox_pixels.uby, this.bbox_pixels.lby);
-        return [x * this._dpr, y * this._dpr];
+        return [x * this.dpr, y * this.dpr];
     }
 
     pix_to_data(_, x, y) {
-        var x_data = linear_interp(x / this._dpr, this.bbox_pixels.lbx, this.bbox_pixels.ubx, this.bbox_data.lbx, this.bbox_data.ubx);
-        var y_data = linear_interp(y / this._dpr, this.bbox_pixels.uby, this.bbox_pixels.lby, this.bbox_data.lby, this.bbox_data.uby);
+        var x_data = linear_interp(x / this.dpr, this.bbox_pixels.lbx, this.bbox_pixels.ubx, this.bbox_data.lbx, this.bbox_data.ubx);
+        var y_data = linear_interp(y / this.dpr, this.bbox_pixels.uby, this.bbox_pixels.lby, this.bbox_data.lby, this.bbox_data.uby);
         return [x_data, y_data];
     }
 
